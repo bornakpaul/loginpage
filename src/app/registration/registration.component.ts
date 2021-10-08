90af8bdfbf06daf8ce1f424add41246e47ce9c96
Bornak <bornakpaul@gmail.com>
2021-10-08 13:19:06 +0530

    new

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  username:string | undefined;
+  password:string | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  LoginUser(){
+    if(this.username == "Admin" && this.password == "admin123"){
+      console.log("Welcome Admin");
+    }
   }
 
 }
