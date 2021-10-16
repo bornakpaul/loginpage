743fe73ec3e384280b93380b08abd7e0b28453aa
Bornak <bornakpaul@gmail.com>
2021-10-16 11:28:09 +0530

    localstorage _ formbuilder

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -10,6 +10,8 @@ export class RegistrationComponent {
 
   title = 'localStorage';
   registrationForm!: any;
+  // username:string | undefined;
+  // password:string | undefined;
 
   constructor(private fb: FormBuilder) { }
 
@@ -23,42 +25,30 @@ export class RegistrationComponent {
       email: ['',[Validators.required, Validators.email]]
     });
   }
-  onSubmit(){
-    localStorage.setItem("users", JSON.stringify(this.registrationForm.value));
-  }
+  //onSubmit(){
+  //localStorage.setItem("users", JSON.stringify(this.registrationForm.value));
+  //}
+
+RegisterUser(){
+
+  if(localStorage.getItem('users') == null){
+    localStorage.setItem('users','[]');
 }
 
-  // RegisterUser(){
-    // if(this.username == "Admin" && this.password == "admin123"){
-    //   console.log("Welcome Admin");
-    // }
+if(localStorage.getItem('username') == null){
+  localStorage.setItem('username','[]');
+}
 
-//   let user = this.username;
-//   let pass = this.password;
-
-//   let myObj = {
-//     user: user,
-//     pass: pass,
-// };
-
-  // if(localStorage.getItem('users') == null){
-  //   localStorage.setItem('users','[]');
-// }
-
-// if(localStorage.getItem('username') == null){
-//   localStorage.setItem('username','[]');
-// }
-
-// let old_data = JSON.parse(localStorage.getItem('users')!);
-// old_data.push();
+let old_data = JSON.parse(localStorage.getItem('users')!);
+old_data.push(this.registrationForm.value);
 
 // let old_username = JSON.parse(localStorage.getItem('username')!);
 // old_username.push(user);
 
-// let myObj_serialized = JSON.stringify(old_data);
+let myObj_serialized = JSON.stringify(old_data);
 // let username_serialized = JSON.stringify(old_username);
 
-// localStorage.setItem('users', myObj_serialized);
+localStorage.setItem('users', myObj_serialized);
 // localStorage.setItem('username', username_serialized);
 
 // let myObj_deserialized = JSON.parse(localStorage.getItem('users')!);
@@ -67,11 +57,9 @@ export class RegistrationComponent {
 
 // const loginDeets = JSON.parse(localStorage.getItem('users')!);
 // const loginDeets_username = JSON.parse(localStorage.getItem('username')!);
-// // console.log(loginDeets_username);
+// console.log(loginDeets_username);
 // console.log(loginDeets);
-// }
-// }
-//}
+}
   
-
+}
 
